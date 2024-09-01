1d7140d512a06ee1486676098a0dec9e1cc0162b
RitikAnand02 <ritikanand.02062000@gmail.com>
2024-09-01 22:53:57 +0530

    update

--- Naresh it lab code/262revword.c
+++ Naresh it lab code/262revword.c
@@ -22,3 +22,40 @@ int main()
 
     return 0;
 }
+
+
+//Method -2....
+
+// #include <stdio.h>
+// int main()
+// {
+//   char str[100];
+//   printf("Enter string : ");
+//   gets(str);
+
+//   int len = 0, sIdx, lIdx, j;
+//   for (int i = 0; str[i] != '\0'; i++)
+//   {
+//     len++;
+//   }
+
+//   for (int i = 0; i < len; i++)
+//   {
+//     if (str[i] != ' ')
+//     {
+//       sIdx = i, j;
+//       for (j = sIdx; str[j] != ' ' && str[j] != '\0'; j++)
+//       {
+//       }
+//       lIdx = j - 1;
+//       for (int k = lIdx; k >= sIdx; k--)
+//       {
+//         printf("%c", str[k]);
+//       }
+//       printf(" ");
+//       i = lIdx;
+//     }
+//   }
+
+//   return 0;
+// }
